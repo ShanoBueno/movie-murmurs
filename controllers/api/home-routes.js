c0a6cb24af09236ae26c697d9c1570e0f8131f54
Shane <shane.messmer1@gmail.com>
2021-07-28 18:56:18 -0400

    more fixes

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User } = require('../../models');
+const { Blog, User } = require('../../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {

9ff7f319ef7e1253f62387264dde1609d05bd253
Shane <shane.messmer1@gmail.com>
2021-07-28 19:28:32 -0400

    removes vote stuff

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
       'movie',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      
     ],
     include: [
      

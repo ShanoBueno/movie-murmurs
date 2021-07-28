14b81f8f415db02111aabf4df77c0d2dd71b6f86
Shane <shane.messmer1@gmail.com>
2021-07-28 16:38:58 -0400

    adds js files

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+app.set('view engine', 'handlebars');
+
+
+app.engine('handlebars', hbs.engine);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('movie-murmurs/controllers'));
+
+
 
 // turn on routes
 app.use(routes);
